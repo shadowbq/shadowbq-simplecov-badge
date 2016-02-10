713e59ffa82846353d86d139a6c1743f80886af8
shadowbq <shadowbq@gmail.com>
2016-02-10 13:03:58 -0500

    incorrect time recorded

--- lib/shadowbq-simplecov-badge/formatter.rb
+++ lib/shadowbq-simplecov-badge/formatter.rb
@@ -1,6 +1,6 @@
 ImageMagickError = Class.new(StandardError)
-  
-class SimpleCov::Formatter::ShadowbqBadgeFormatter  
+
+class SimpleCov::Formatter::ShadowbqBadgeFormatter
   # Set up config variables.
   options = {:badge_title => 'TEST COVERAGE', :generate_groups => true, :timestamp => false, :green => '#4fb151',
             :yellow => '#ded443', :red => '#a23e3f', :number_font => 'Helvetica-Narrow-Bold',
@@ -10,7 +10,7 @@ class SimpleCov::Formatter::ShadowbqBadgeFormatter
             :group_name_font => 'Helvetica-Bold', :group_name_font_size => 15,
             :group_badge_height => 22, :group_strength_foreground => false, :color_code_title => true,
             :group_color_code_title => true}
-    
+
   # set up class variables and getters/setters
   options.each do |opt,v|
     set = "@@#{opt} = v"
@@ -20,11 +20,11 @@ class SimpleCov::Formatter::ShadowbqBadgeFormatter
     eval(getter)
     eval(setter)
   end
-  
+
   def initialize(output = nil)
     @output = output || STDOUT
   end
-  
+
   def format(result)
     begin
       check_imagemagick
@@ -39,13 +39,13 @@ class SimpleCov::Formatter::ShadowbqBadgeFormatter
   end
 
   private
-  
+
   def generate_header_badge(result)
     overall_cov = result.source_files.covered_percent.round(0)
     overall_strength = result.covered_strength.round(0)
     generator(overall_cov, overall_strength, false)
   end
-  
+
   def generate_group_badges(result)
     result.groups.each do |name, files|
       cov = files.covered_percent.round(0)
@@ -53,7 +53,7 @@ class SimpleCov::Formatter::ShadowbqBadgeFormatter
       generator(cov, strength, name)
     end
   end
-  
+
   def generator(cov, strength, group)
     command = []
     command[0] = """
@@ -115,18 +115,18 @@ class SimpleCov::Formatter::ShadowbqBadgeFormatter
       system("rm #{output_path}/tmp.png")
     end
   end
-  
+
   def generate_timestamp
     timestamp_cmd = """
       convert #{output_path}/coverage-badge.png -alpha set -bordercolor none -border 3 \
       -gravity North -chop 0x3 \
       -gravity East -chop 3x0 \
-      -gravity West -chop 3x0 \\( -background none -font 'Helvetica' label:'Generated #{Time.now.strftime('%m-%d-%y %H:%M UTC')}' \\) -background none -gravity center -append #{output_path}/coverage-badge.png
+      -gravity West -chop 3x0 \\( -background none -font 'Helvetica' label:'Generated #{Time.now.utc.strftime('%m-%d-%y %H:%M UTC')}' \\) -background none -gravity center -append #{output_path}/coverage-badge.png
     """
     output = `#{timestamp_cmd}`
     check_status(output)
   end
-  
+
   # getter method for config variables - abstracts group or main badge from generator
   def get_config(name, group)
     if group
@@ -135,22 +135,22 @@ class SimpleCov::Formatter::ShadowbqBadgeFormatter
       eval("@@#{name}")
     end
   end
-  
+
   # checks if imagemagick is installed and working
   def check_imagemagick
     output = `convert --version`
     raise ImageMagickError, "ImageMagick doesn't appear to be installed." unless $?.to_i == 0
   end
-  
+
   # Checks exit status after running a command with backtick
   def check_status(output)
     raise ImageMagickError, "ImageMagick exited with an error. It said:\n #{output}" unless $?.to_i == 0
   end
-  
+
   def output_message(result)
     "Coverage badge generated for #{result.command_name} to #{output_path}."
   end
-  
+
   def output_path
     SimpleCov.coverage_path
   end
@@ -168,7 +168,7 @@ class SimpleCov::Formatter::ShadowbqBadgeFormatter
       @@red
     end
   end
-  
+
   def title_foreground(cov, strength, foreground, use)
     if !foreground or !use
       'white'
@@ -180,7 +180,7 @@ class SimpleCov::Formatter::ShadowbqBadgeFormatter
       @@red
     end
   end
-  
+
   def strength_background(strength, foreground)
     if foreground
       'transparent'
@@ -188,7 +188,7 @@ class SimpleCov::Formatter::ShadowbqBadgeFormatter
       strength_color(strength)
     end
   end
-  
+
   def strength_foreground(strength, foreground)
     unless foreground
       'white'
@@ -196,7 +196,7 @@ class SimpleCov::Formatter::ShadowbqBadgeFormatter
       strength_color(strength)
     end
   end
-  
+
   def coverage_color(covered_percent)
     if covered_percent > 90
       @@green
--- lib/shadowbq-simplecov-badge/version.rb
+++ lib/shadowbq-simplecov-badge/version.rb
@@ -1,7 +1,7 @@
 module SimpleCov
   module Formatter
     class ShadowbqBadgeFormatter
-      VERSION = "2.0.4"
+      VERSION = "2.0.5"
     end
   end
 end
